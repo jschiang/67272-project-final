2ec8ab305f8cf1a18d0e705dfe1e61feeb789040
Jeremy <jschiang@andrew.cmu.edu>
2013-04-28 18:57:06 -0400

    starting to build dojo crud functionality

---
+++ app/controllers/dojos_controller.rb
@@ -0,0 +1,49 @@
+class DojosController < ApplicationController
+
+  def index
+    @dojos = Dojo.active.alphabetical.paginate(:page => params[:page]).per_page(10)
+    @inactive_dojos = Dojo.inactive.alphabetical.paginate(:page => params[:page]).per_page(10)
+  end
+
+  def show
+    @dojo = Dojo.find(params[:id])
+    @students = @dojo.students.all.paginate(:page => params[:page]).per_page(10)
+  end
+  
+  def new
+    @dojo = Dojo.new
+  end
+
+  def edit
+    @dojo = Dojo.find(params[:id])
+  end
+
+  def create
+    @dojo = Dojo.new(params[:dojo])
+    if @dojo.save
+      # if saved to database
+      flash[:notice] = "Successfully created #{@dojo.name}."
+      redirect_to @dojo # go to show dojo page
+    else
+      # return to the 'new' form
+      render :action => 'new'
+    end
+  end
+
+  def update
+    @dojo = Dojo.find(params[:id])
+    if @dojo.update_attributes(params[:dojo])
+      flash[:notice] = "Successfully updated #{@dojo.name}."
+      redirect_to @dojo
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @dojo = Dojo.find(params[:id])
+    @dojo.destroy
+    flash[:notice] = "Successfully removed #{@dojo.name} from karate tournament system"
+    redirect_to dojos_url
+  end
+end
