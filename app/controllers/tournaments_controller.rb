abb04d9ff9e312318c31ebc4b35c2f28e3396a23
Jeremy <jschiang@andrew.cmu.edu>
2013-05-01 13:10:54 -0400

    started building tournament views

---
+++ app/controllers/tournaments_controller.rb
@@ -0,0 +1,48 @@
+class TournamentsController < ApplicationController
+
+  def index
+    @tournaments = Tournament.active.alphabetical.paginate(:page => params[:page]).per_page(10)
+  end
+
+  def show
+    @tournament = Tournament.find(params[:id])
+    @sections = @tournament.sections.paginate(:page => params[:page]).per_page(10)
+  end
+  
+  def new
+    @tournament = Tournament.new
+  end
+
+  def edit
+    @tournament = Tournament.find(params[:id])
+  end
+
+  def create
+    @tournament = Tournament.new(params[:tournament])
+    if @tournament.save
+      # if saved to database
+      flash[:notice] = "Successfully created #{@tournament.name} tournament."
+      redirect_to @tournament # go to show tournament page
+    else
+      # return to the 'new' form
+      render :action => 'new'
+    end
+  end
+
+  def update
+    @tournament = Tournament.find(params[:id])
+    if @tournament.update_attributes(params[:tournament])
+      flash[:notice] = "Successfully updated #{@tournament.name} tournament."
+      redirect_to @tournament
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @tournament = Tournament.find(params[:id])
+    @tournament.destroy
+    flash[:notice] = "Successfully removed #{@tournament.name} from karate tournament system"
+    redirect_to tournaments_url
+  end
+end
