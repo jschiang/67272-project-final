a9ba5057d3587e153a6bd94a40976d61d4bbf922
Jeremy <jschiang@andrew.cmu.edu>
2013-05-05 00:54:21 -0400

    nested user form shows up

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,63 @@
+class UsersController < ApplicationController
+  
+  before_filter :check_login
+  authorize_resource
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+    authorize! :new, @user
+    @user.student_id = params[:student_id] unless params[:student_id].nil?
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    authorize! :edit, @user
+      if params[:from] == 'student'
+        redirect_to student_path(@user.student)
+      else
+        redirect_to students_path
+      end
+    end
+  end
+
+  def create
+    @user = User.new(params[:user])
+    authorize! :create, @user
+    @user.created_by = current_user.id
+    if @user.save
+      # if saved to database
+      flash[:notice] = "Account for #{@user.student.proper_name} has been created."
+      redirect_to student_path(@user.student)
+    else
+      # return to the 'new' form
+      render :action => 'new'
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    authorize! :update, @task
+    
+    if @task.save!
+      flash[:notice] = "Account for #{@user.student.proper_name} has been updated."
+      redirect_to student_path(@user.student)
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    authorize! :destroy, @user
+    @user.destroy
+    flash[:notice] = "Successfully removed account for #{@user.student.proper_name}."
+    redirect_to students_url
+  end
+
+end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,7 @@
 class Student < ActiveRecord::Base
-  attr_accessible :first_name, :last_name, :date_of_birth, :rank, :phone, :waiver_signed, :active
+  attr_accessible :first_name, :last_name, :date_of_birth, :rank, :phone, :waiver_signed, :active, :user_attributes
+
+
   
   # Constants
   RANKS = [['Tenth Gup', 1],['Ninth Gup', 2],['Eighth Gup', 3],['Seventh Gup', 4],['Sixth Gup', 5],
@@ -18,6 +20,8 @@ class Student < ActiveRecord::Base
   has_many :tournaments, :through => :sections
   has_many :dojo_students
   has_many :dojos, :through => :dojo_students
+
+  accepts_nested_attributes_for :user, :reject_if => lambda { |user| user[:email].blank? }, :allow_destroy => true
   
   # Scopes
   scope :alphabetical, order('last_name, first_name')
