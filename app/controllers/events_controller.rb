6d7fef4673ac09f688ef01e6c085af261cc4b78c
Jeremy <jschiang@andrew.cmu.edu>
2013-05-07 14:32:31 -0400

    changed some event authorization stuff

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
 
-  before_filter :check_login, :except => [:index, :show]
+  before_filter :check_login
 
   def index
     @events = Event.alphabetical.paginate(:page => params[:page]).per_page(8)
